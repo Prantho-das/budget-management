8c078f73756acc21c0cd1e556f636bc8afe19600
northbengal <northbengal22@gmail.com>
2026-01-10 19:00:35 +0600

    update

--- resources/views/livewire/setup/expenses.blade.php
+++ resources/views/livewire/setup/expenses.blade.php
@@ -1,4 +1,4 @@
-<div class="unitoffice-entry-table budget-status-table">
+<div class="unitoffice-entry-table expense-table">
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
