80a407e23a28ac6c1709b89997e1b51d1a70cdf4
Prantho-das <pranthokumardas786@gmail.com>
2026-02-02 23:09:19 +0600

    number to bangla

--- resources/views/livewire/budget-distribution/budget-distribution-entry.blade.php
+++ resources/views/livewire/budget-distribution/budget-distribution-entry.blade.php
@@ -52,6 +52,7 @@ $numto = new Rakibhstu\Banglanumber\NumberToBangla();
                                                                         @php
                                     $fiscial_year_explode = explode($fy->name,'-');
                                                                         @endphp
+                                                                        {{ dd($fiscial_year_explode) }}
                                                                         {{ $numto->bnNum($fiscial_year_explode[0]) }}-{{ $numto->bnNum($fiscial_year_explode[1]) }}
                                                                     </option>
                                 @endforeach
