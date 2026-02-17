d5f58fb7ae946f488d20236b4b934ae1a523c87e
Prantho-das <pranthokumardas786@gmail.com>
2026-02-18 03:34:09 +0600

    canges by broter

--- resources/views/layouts/skot.blade.php
+++ resources/views/layouts/skot.blade.php
@@ -80,7 +80,7 @@
 
                     <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect"
                         id="vertical-menu-btn">
-                        <i class="fas fa-bars"></i>
+                        <i class="bx bx-menu"></i>
                     </button>
 
                 </div>
