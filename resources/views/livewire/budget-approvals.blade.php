f6bfb02cd1d41ddd6cf1e440dea6fb9cd411ba3d
Prantho Kumar Das <pranthokumardas786@gmail.com>
2026-07-22 10:57:06 +0600

    feat: add Livewire budget approvals component with detailed review and print functionality

--- resources/views/livewire/budget-approvals.blade.php
+++ resources/views/livewire/budget-approvals.blade.php
@@ -1,4 +1,5 @@
 <div>
+    @if($viewMode === 'detail')
     <style>
         /* Screen styles: Hide print layout on screen */
         #formal-print-report {
@@ -104,6 +105,7 @@
             }
         }
     </style>
+    @endif
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
